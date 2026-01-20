9731df0dd76101480754602f0579000135e7e05b
Diêgo de Barros <debarrosdiego415@gmail.com>
2026-01-20 11:30:13 -0300

    feat(Resolução): adiciona execução principal do exercício Petshop

---
+++ exercicios/3 - Resolução Java Abstração, Classes e Encapsulamentos/src/Exec03MaquinaBanhoPetshop.java
@@ -0,0 +1,9 @@
+import menu.MenuBase;
+import menu.MenuMaquinaBanho;
+
+public class Exec03MaquinaBanhoPetshop {
+    public static void main(String[] args){
+        MenuBase menu = new MenuMaquinaBanho();
+        menu.executar();
+    }
+}
