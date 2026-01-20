ab53bd7c47a091aa1e7ae62993a4d8e24e8349cf
Diêgo de Barros <debarrosdiego415@gmail.com>
2026-01-20 11:27:14 -0300

    feat(Resolução-3): adiciona execução principal do exercício Carro

---
+++ exercicios/3 - Resolução Java Abstração, Classes e Encapsulamentos/src/Exec02Carro.java
@@ -0,0 +1,9 @@
+import menu.MenuCarro;
+import menu.MenuBase;
+
+public class Exec02Carro {
+    public static void main (String[] args){
+        MenuBase menu = new MenuCarro();
+        menu.executar();
+    }
+}
